cd13f3bb3be3c29b185ddfedd1a6e80b4f287624
Elias Bermudez <de.bermudez13@gmail.com>
2020-07-28 09:44:27 -0700

    Update copyright

--- pyprof/parse/nsight.py
+++ pyprof/parse/nsight.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2020, Aditya Agrawal.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
