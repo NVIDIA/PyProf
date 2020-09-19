983343e441ec39668776ee6414735c6a6916e43c
Aditya Agrawal <aditya.iitb@gmail.com>
2020-09-19 07:04:09 -0700

    Using tensor methods for pointwise kernels.
    Added a helper function to return the broadcast shape.
    
    Signed-off-by: Aditya Agrawal <aditya.iitb@gmail.com>

--- pyprof/prof/pointwise.py
+++ pyprof/prof/pointwise.py
@@ -16,107 +16,84 @@
 # limitations under the License.
 
 import numpy as np
-from collections import OrderedDict
-from .utility import Utility
 from .base import OperatorLayerBase
-
+from .tensor import Tensor
+from functools import reduce
+import operator
 
 class Pointwise(OperatorLayerBase):
 
-    ops = []
-    ops += ["__abs__", "__neg__", "__invert__"]
-    ops += ["__add__", "__sub__", "__mul__", "__floordiv__", "__truediv__", "__pow__", "__mod__"]
-    ops += ["__radd__", "__rsub__", "__rmul__", "__rdiv__", "__rtruediv__", "__rfloordiv__", "__rpow__"]
-    ops += [
-        "__iadd__",
-        "__isub__",
-        "__imul__",
-        "__itruediv__",
-    ]
-    ops += [
-        "__lt__",
-        "__gt__",
-        "__ge__",
-        "__le__",
-        "__eq__",
-        "__ne__",
-    ]
-    ops += [
-        "lt",
-        "lt_",
-        "gt",
-        "gt_",
-        "ge",
-        "ge_",
-        "le",
-        "le_",
-        "eq",
-        "eq_",
-        "ne",
-        "ne_",
-    ]
-    ops += ["__and__", "__or__", "__xor__", "__lshift__", "__rshift__"]
-    ops += ["__iand__", "__ior__", "__ixor__", "__ilshift__", "__irshift__"]
-    ops += ["abs", "abs_", "neg", "neg_"]
-    ops += [
-        "add",
-        "add_",
-        "div",
-        "div_",
-        "mul",
-        "mul_",
-        "reciprocal",
-        "reciprocal_",
-        "remainder",
-        "remainder_",
-        "sub",
-        "sub_",
-    ]
-    ops += ["addcdiv", "addcdiv_", "addcmul", "addcmul_"]
-    ops += [
-        "exp",
-        "exp_",
-        "exp1m",
-        "exp1m_",
-        "log",
-        "log_",
-        "log10",
-        "log10_",
-        "log1p",
-        "log1p_",
-        "log2",
-        "log2_",
-        "pow",
-        "pow_",
-        "rsqrt",
-        "rsqrt_",
-        "sqrt",
-        "sqrt_",
-    ]
-    ops += [
-        "ceil", "ceil_", "clamp", "clamp_", "floor", "floor_", "fmod", "fmod_", "frac", "frac_", "round", "round_",
-        "sign", "sign_", "trunc", "trunc_"
-    ]
-    ops += [
-        "acos", "acos_", "asin", "asin_", "atan", "atan_", "atan2", "atan2_", "cos", "cos_", "cosh", "cosh_", "sin",
-        "sin_", "sinh", "sinh_", "tan", "tan_", "sigmoid", "sigmoid_", "tanh", "tanh_"
-    ]
-    ops += [
-        "digamma",
-        "erf",
-        "erf_",
-        "erfc",
-        "erfc_",
-        "erfinv",
-        "erfinv_",
-        "lerp",
-        "lerp_",
-        "mvlgamma",
-    ]
+    # TODO: Add more operators.
+    # TODO: Determining the output dtype is tricky.
+    # TODO: Refine calculations based on direction.
+    # TODO: Refine calculations for non-arithmetic ops.
 
-    @staticmethod
-    def foo(d):
-        return d['name'], d['type'], d['shape'], d['dtype']
+    # Memset
+    memset = ["zero_", "zeros", "zeros_like", "ones", "ones_like"]
+
+    # Unary
+    unary = ["abs", "abs_", "neg", "neg_", "reciprocal", "reciprocal_"]
+    unary += ["__abs__", "__neg__"]
+
+    # Unary bitwise
+    unary += ["__invert__"]
+
+    # Exponential and log (unary)
+    exp_log = ["exp", "exp_", "exp1m", "exp1m_", "log", "log_",
+               "log10", "log10_", "log1p", "log1p_", "log2", "log2_"]
+
+    # Sqrt (unary)
+    sqrt = ["rsqrt", "rsqrt_", "sqrt", "sqrt_"]
+
+    # Representation (unary)
+    representation = ["ceil", "ceil_", "clamp", "clamp_", "floor", "floor_",
+                      "frac", "frac_", "round", "round_", "sign", "sign_",
+                      "trunc", "trunc_"]
+
+    # Trigonometric and transcendental (unary)
+    trig_trans = ["acos", "acos_", "asin", "asin_", "atan", "atan_",
+                  "atan2", "atan2_", "cos", "cos_", "cosh", "cosh_",
+                  "sin", "sin_", "sinh", "sinh_", "tan", "tan_",
+                  "sigmoid", "sigmoid_", "tanh", "tanh_"]
+
+    # Error (unary)
+    error = ["erf", "erf_", "erfc", "erfc_", "erfinv", "erfinv_"]
+
+    # Binary
+    binary = ["add", "add_", "div", "div_", "mul", "mul_",
+              "remainder", "remainder_", "sub", "sub_"]
+    binary += ["__add__", "__sub__", "__mul__", "__floordiv__",
+               "__truediv__", "__mod__"]
+    binary += ["__radd__", "__rsub__", "__rmul__", "__rdiv__",
+               "__rtruediv__", "__rfloordiv__"]
+    binary += ["fmod", "fmod_"]
+
+    # Binary inplace
+    ibinary = ["__iadd__", "__isub__", "__imul__", "__itruediv__"]
+
+    # Power (binary)
+    power = ["pow", "pow_", "__pow__", "__rpow__"]
+
+    # Comparison (binary)
+    comp = ["lt", "lt_", "gt", "gt_", "ge", "ge_", "le", "le_",
+            "eq", "eq_", "ne", "ne_"]
+    comp += ["__lt__", "__gt__", "__ge__", "__le__", "__eq__", "__ne__"]
+
+    # Logical (binary)
+    logical = ["__and__", "__or__", "__xor__", "__lshift__", "__rshift__"]
+
+    # Logical inplace (binary)
+    ilogical = ["__iand__", "__ior__", "__ixor__", "__ilshift__", "__irshift__"]
+
+    # Ternary
+    ternary = ["addcdiv", "addcdiv_", "addcmul", "addcmul_"]
+
+    # Misc
+    misc = ["digamma", "lerp", "lerp_", "mvlgamma"]
+
+    ops = memset + unary + binary + ibinary + comp + logical + ilogical + \
+          ternary + exp_log + power + sqrt + representation + trig_trans + \
+          error + misc
 
     def __init__(self, d):
         marker = eval(d.argMarker[0])
@@ -125,133 +102,118 @@ class Pointwise(OperatorLayerBase):
         args = marker['args']
 
         self.marker = marker
-        self.mod_ = mod
-        self.op_ = op
+        self._mod = mod
+        self._op = op
         self.args = args
 
         self.dir = d.dir
         assert (d.dir in ["fprop", "bprop"])
         assert (op in Pointwise.ops)
 
-        #Filter out all named parameters (kwargs).
-        #This might require revisiting in future.
+        # Filter out all named parameters (kwargs).
+        # This might require revisiting in future.
         args = list(filter(lambda x: x['name'] == "", args))
 
-        #Filter out non tensors
-        args = list(filter(lambda x: x['type'] == "tensor", args))
+        # Filter out non tensors
+        #args = list(filter(lambda x: x['type'] == "tensor", args))
 
-        if (len(args) == 0):
-            self.shape = [(1, )]
-            self.type = "float32"  #FIX
+        assert (len(args) <= 4)
+        self.inp = []
 
-        elif (len(args) == 1):
-            in0 = args[0]
-            _, t0, s0, dt0 = Pointwise.foo(in0)
-            assert (t0 == "tensor")
-            self.shape = [
-                s0,
-            ]
-            self.type = dt0
+        for arg in args:
+            if (arg['type'] == "tensor"):
+                t = Tensor(arg['shape'], arg['dtype'])
+            else: # scalar
+                t = Tensor([], arg['type'])
 
-        elif (len(args) == 2):
-            in0, in1 = args
-            _, t0, s0, dt0 = Pointwise.foo(in0)
-            _, t1, s1, dt1 = Pointwise.foo(in1)
-            assert (t0 == t1 == "tensor")
-            # TODO: review assertions given that dtypes do not need to be equal
-            # tracked here: https://github.com/NVIDIA/PyProf/issues/26
-            self.shape = [s0, s1]
-            self.type = dt0
-
-        elif (len(args) == 3):
-            in0, in1, in2 = args
-            _, t0, s0, dt0 = Pointwise.foo(in0)
-            _, t1, s1, dt1 = Pointwise.foo(in1)
-            _, t2, s2, dt2 = Pointwise.foo(in2)
-            assert (t0 == t1 == t2 == "tensor")
-            self.shape = [s0, s1, s2]
-            self.type = dt0
-        else:
-            assert False
-        return
+            self.inp.append(t)
 
     def params(self):
-        p = OrderedDict([('T', self.shape), ('type', self.type)])
-        return p
+        return ";".join([str(t) for t in self.inp])
 
     def tc(self):
         return "-"
 
     def op(self):
-        return self.op_
+        return self._op
 
     def mod(self):
-        return self.mod_
+        return self._mod
 
-    def elems(self):
-        tensor = self.shape
-        t = self.type
+    def bytes_flops(self):
+        b = f = 0
 
-        if (len(tensor) == 1):
-            elems = 2 * Utility.numElems(tensor[0])
-        elif (len(tensor) == 2):
-            if (tensor[0] == tensor[1]):  # same shape
-                elems = Utility.numElems(tensor[0])
-                if self.dir == "fprop":
-                    elems *= 3
-                else:
-                    if (self.op_ in ["add", "__add__", "sub", "__sub__", "__isub__"]):
-                        elems *= 2
-                    elif (self.op_ in ["__mul__", "__rmul__", "div", "__truediv__"]):
-                        elems *= 3
-                    else:
-                        assert False
-            else:  #check for broadcast conditions
-                array1 = np.empty(list(tensor[0]))
-                array2 = np.empty(list(tensor[1]))
-                try:
-                    out = np.broadcast(array1, array2).shape
-                except:
-                    assert False
+        # Memset
+        if self.op() in Pointwise.memset:
+            assert (len(self.inp) == 1)
+            b = self.inp[0].bytes
+            f = 0
 
-                elems = Utility.numElems(tensor[0])
-                elems += Utility.numElems(tensor[1])
-                elems += Utility.numElems(out)
-                #TODO bprop
-        elif (len(tensor) == 3):
-            if (tensor[0] == tensor[1] == tensor[2]):  #same shape
-                elems = Utility.numElems(tensor[0])
-                elems *= 4
+        # Unary
+        elif self.op() in Pointwise.unary + Pointwise.representation:
+            assert (len(self.inp) == 1)
+            b = 2 * self.inp[0].bytes
+            f = self.inp[0].size
+
+        elif self.op() in Pointwise.exp_log + Pointwise.trig_trans + \
+                Pointwise.sqrt + Pointwise.error:
+            assert (len(self.inp) == 1)
+            b = 2 * self.inp[0].bytes
+            f = self.inp[0].size * 20 # estimate
+
+        # Binary
+        elif self.op() in Pointwise.comp + \
+                Pointwise.binary + Pointwise.ibinary + \
+                Pointwise.logical + Pointwise.ilogical:
+
+            assert (len(self.inp) == 2)
+            out = Tensor.broadcast(self.inp)
+
+            if self.dir == "fprop":
+                b = reduce(operator.add, [t.bytes for t in self.inp])
+                # The output of comparison is bool
+                if self.op() in Pointwise.comp:
+                    out = Tensor(out.shape, "bool")
+                b += out.bytes
+                f = out.size
             else:
-                assert False
-        else:
-            assert False
+                if (self.op() in ["add", "__add__", "sub", "__sub__", "__isub__"]):
+                    b = 2 * out.bytes
+                    f = 0
+                elif (self.op() in ["__mul__", "__imul__", "__rmul__", "div", "__truediv__"]):
+                    b = 3 * out.bytes
+                    f = out.size
+                else:
+                    e = f'{self.op()} bprop not supported yet. Please file a bug.'
+                    assert False, e
 
-        return elems
+        elif self.op() in Pointwise.power:
+            assert (len(self.inp) == 2)
+            out = Tensor.broadcast(self.inp)
+            b = reduce(operator.add, [t.bytes for t in self.inp])
+            b += out.bytes
+            f = out.size * 20 # estimate
+
+        # Ternary
+        elif self.op() in Pointwise.ternary:
+            # Remove scalars
+            tensors = list(filter(lambda x: x.shape != [], self.inp))
+            assert len(tensors) == 3
+            out = Tensor.broadcast(tensors)
+            b = reduce(operator.add, [t.bytes for t in tensors])
+            b += out.bytes
+            f = 3 * out.size
+
+        else:
+            e = f'{self.op()} not supported yet. Please file a bug.'
+            assert False, e
+
+        return b, f
 
     def bytes(self):
-        return self.elems() * Utility.typeToBytes(self.type)
+        b, f = self.bytes_flops()
+        return b
 
     def flops(self):
-        # Note: some cases may still be missing.
-
-        f = 0
-        if self.op_ in ["__abs__", "__neg__", "__add__", "__sub__", "__mul__", "__radd__", "__rmul__", "__iadd__",
-                        "__isub__", "__imul__", "__itruediv__", "abs", "abs_", "neg", "neg_", "add", "add_", "div",
-                        "div_", "mul", "mul_", "sub", "sub_", "exp", "exp_", "sign", "sign_", "trunc", "trunc_", "sin",
-                        "sin_", "cos", "cos_", "sinh", "sinh_", "cosh", "cosh_", "sqrt", "sqrt_", "rsqrt", "rsqrt_",
-                        "__lt__", "__gt__", "__ge__", "__le__", "__eq__", "__ne__", "lt", "lt_", "gt", "gt_", "ge",
-                        "ge_", "le", "le_", "eq", "eq_", "ne", "ne_", "ceil", "ceil_", "clamp", "clamp_", "floor",
-                        "floor_", "round", "sign", "sign_", "trunc", "trunc_"]:
-            # We're counting only one operand, not two (2 operands, 1 op)
-            f = self.elems() / 2
-        elif self.op_ in ["fmod", "fmod_"]:
-            f = self.elems()
-        elif self.op_ in ["tanh", "tanh_", "sigmoid", "sigmoid_", "log", "log_", "log2", "log2_", "log10", "log10_"]:
-            f = self.elems() * 2
-        elif self.op_ in ["asin", "asin_", "acos", "acos_", "atan", "atan_"]:
-            # no intrinsic, hence slow execution
-            # surprisingly, asin/acos and atan were all the same (via nvprof measurement)
-            f = self.elems() * 10
-
+        b, f = self.bytes_flops()
         return f
