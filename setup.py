af178bd0e7f5dfd3046d3fee340180ff71f19c63
Elias Bermudez <6505145+debermudez@users.noreply.github.com>
2020-10-07 16:22:07 -0700

    Add pip package (#92)
    * Update setup.py for pip package
    
    * Add proper versioning to setup.py
    
    * Add manifest.in file to include pyprof example directory
    
    * Add requirements_nsys file

--- setup.py
+++ setup.py
@@ -15,16 +15,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as f:
-    required = f.read().splitlines()
-
 with open('VERSION', 'r') as f:
-    version = f.read()
+    version = f.read()[:-4]
+
+
+def req_file(filename, folder="requirements"):
+    with open(os.path.join(folder, filename)) as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters
+    # Example: `\n` at the end of each line
+    return [x.strip() for x in content]
+
+
+install_requires = req_file("requirements.txt")
+
+extras_require = {
+    # User packages
+    'nsys': req_file("requirements_nsys.txt"),
+}
+
 
 setup(
-    name='pyprof',
+    name='nvidia-pyprof',
     version=version,
     packages=find_packages(),
     author="Aditya Agrawal,Marek Kolodziej",
@@ -32,7 +47,28 @@ setup(
     maintainer="Elias Bermudez",
     maintainer_email="dbermudez13@gmail.com",
     url="https://github.com/NVIDIA/PyProf",
+    download_url="https://github.com/NVIDIA/PyProf",
     license="BSD 3-Clause License",
     description='NVIDIA Pytorch Profiler',
-    install_requires=required,
+    classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Information Technology',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Utilities',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Environment :: Console',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+    ],
+    keywords='nvidia, profiling, deep learning, ' \
+             'machine learning, supervised learning, ' \
+             'unsupervised learning, reinforcement learning, ',
+    platforms=["Linux"],
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=extras_require,
 )
